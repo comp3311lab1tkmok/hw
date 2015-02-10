331f71f8643a58f2f22c09c8f61f1f93bcd0050f
comp3311lab1tkmok <comp3311lab1tkmok@gmail.com>
2015-02-10 13:00:14 +0800

    ser

--- hw/src/hw/hw.java
+++ hw/src/hw/hw.java
@@ -5,5 +5,7 @@ public class hw {
 	public static void main(String[] args){
 		System.out.println("HW");
 		System.out.println("testing update!!");
+		System.out.println("make some change");
+
 	}
 }
